636c046e6448e3e5efe3204df2b191cb67f5ce4f
Dmytro Voronkevych <dmitry.voronkevich@gmail.com>
2026-09-14 13:00:05 +0100

    no-mistakes: apply CI fixes

--- retype/services/autosave.py
+++ retype/services/autosave.py
@@ -6,10 +6,10 @@ from typing import TYPE_CHECKING
 class IdleSignal(QObject):
     idle = pyqtSignal()
 
-    def __init__(self, console, interval):
-        # type: (IdleSignal, Console, int) -> None
-        super().__init__()
-        self.timer = QTimer()
+    def __init__(self, console, interval, parent=None):
+        # type: (IdleSignal, Console, int, QObject | None) -> None
+        super().__init__(parent)
+        self.timer = QTimer(self)
         self.timer.setInterval(interval)
         self.timer.timeout.connect(self.emitIdle)
         console.textEdited.connect(self.onUpdate)
@@ -27,11 +27,11 @@ class IdleSignal(QObject):
 class Autosave(QObject):
     save = pyqtSignal()
 
-    def __init__(self, console, on=True, interval=5000):
-        # type: (Autosave, Console, bool, int) -> None
-        super().__init__()
+    def __init__(self, console, on=True, interval=5000, parent=None):
+        # type: (Autosave, Console, bool, int, QObject | None) -> None
+        super().__init__(parent)
         self.on = on
-        self.signal = IdleSignal(console, interval)
+        self.signal = IdleSignal(console, interval, self)
         self.signal.idle.connect(self.maybeEmitSave)
 
     def maybeEmitSave(self):
